6df94450af3a8748ad4767900b947b6e404a9dc2
Josef Zacek <info@josefzacek.com>
2014-01-03 19:29:44 +0000

    main structure and index page done

---
+++ js/scripts.js
@@ -0,0 +1,34 @@
+$( document ).ready(function() {
+    // slideshow
+	$(".rslides").responsiveSlides({
+		auto: true,             // Boolean: Animate automatically, true or false
+		speed: 500,            // Integer: Speed of the transition, in milliseconds
+		timeout: 4000,          // Integer: Time between slide transitions, in milliseconds
+		pager: true,           // Boolean: Show pager, true or false
+		nav: false,             // Boolean: Show navigation, true or false
+		random: false,          // Boolean: Randomize the order of the slides, true or false
+		pause: false,           // Boolean: Pause on hover, true or false
+		pauseControls: true,    // Boolean: Pause when hovering controls, true or false
+		prevText: "Previous",   // String: Text for the "previous" button
+		nextText: "Next",       // String: Text for the "next" button
+		maxwidth: "",           // Integer: Max-width of the slideshow, in pixels
+		navContainer: "",       // Selector: Where controls should be appended to, default is after the 'ul'
+		manualControls: "",     // Selector: Declare custom pager navigation
+		namespace: "rslides",   // String: Change the default namespace used
+		before: function(){},   // Function: Before callback
+		after: function(){}     // Function: After callback
+  	});
+
+		var nav = responsiveNav(".nav-collapse", { // Selector
+		animate: true, // Boolean: Use CSS3 transitions, true or false
+		transition: 250, // Integer: Speed of the transition, in milliseconds
+		label: "Menu", // String: Label for the navigation toggle
+		insert: "after", // String: Insert the toggle before or after the navigation
+		customToggle: "", // Selector: Specify the ID of a custom toggle
+		openPos: "relative", // String: Position of the opened nav, relative or static
+		jsClass: "js", // String: 'JS enabled' class which is added to <html> el
+		init: function(){}, // Function: Init callback
+		open: function(){}, // Function: Open callback
+		close: function(){} // Function: Close callback
+	});
+});
